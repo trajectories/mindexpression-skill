3fad0df8d7e4739db9e7cb56843be3f7905ec24d
trajectories <79078599+trajectories@users.noreply.github.com>
2023-02-26 02:41:55 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ class Mindexpression(MycroftSkill):
     def __init__(self):
         super(Mindexpression, self).__init__(name="Mindexpression")
 
-    @intent_handler('Mindexpression.intent')
+    @intent_handler('mindexpression.intent')
     def handle_mindai(self, message):
         self.speak_dialog('mindexpression')
 
@@ -22,37 +22,37 @@ class Mindexpression(MycroftSkill):
         self.url = self.settings.get('url')
         self.api_key = self.settings.get('api_key')
 
-    def send_message(self, message):
-        headers = {'Authorization': f'Bearer {self.api_key}'}
-        data = {'message': message}
-        response = requests.post(self.url, headers=headers, json=data)
-        if response.status_code == 200:
-            return response.json().get('response')
-        else:
-            logger.warning(
-                f'Mind Expression request failed with status code {response.status_code}')
-            return None
+    # def send_message(self, message):
+    #     headers = {'Authorization': f'Bearer {self.api_key}'}
+    #     data = {'message': message}
+    #     response = requests.post(self.url, headers=headers, json=data)
+    #     if response.status_code == 200:
+    #         return response.json().get('response')
+    #     else:
+    #         logger.warning(
+    #             f'Mind Expression request failed with status code {response.status_code}')
+    #         return None
 
-    def handle(self, message):
-        response = self.send_message(message.data['utterance'])
-        if response is not None:
-            self.speak(response)
+    # def handle(self, message):
+    #     response = self.send_message(message.data['utterance'])
+    #     if response is not None:
+    #         self.speak(response)
 
-    def send_to_mind_expression(self, message):
-        try:
-            ws = create_connection("wss://localhost:8181")
-            utterance = message.data.get('utterance')
-            message = {
-                'type': 'speak',
-                'data': {'utterance': utterance}
-            }
-            ws.send(json.dumps(message))
-            response = ws.recv()
-            logger.info("Received response from Mind Expression: " + response)
-            ws.close()
-            self.bus.emit(Message("speak", {"utterance": response}))
-        except:
-            logger.error("Failed to connect to Mind Expression")
+    # def send_to_mind_expression(self, message):
+    #     try:
+    #         ws = create_connection("wss://localhost:8181")
+    #         utterance = message.data.get('utterance')
+    #         message = {
+    #             'type': 'speak',
+    #             'data': {'utterance': utterance}
+    #         }
+    #         ws.send(json.dumps(message))
+    #         response = ws.recv()
+    #         logger.info("Received response from Mind Expression: " + response)
+    #         ws.close()
+    #         self.bus.emit(Message("speak", {"utterance": response}))
+    #     except:
+    #         logger.error("Failed to connect to Mind Expression")
 
     def stop(self):
         pass
