b7873aae41e563dd716c27c706abfd4b950429e1
trajectories <79078599+trajectories@users.noreply.github.com>
2023-02-26 01:37:53 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,53 +1,60 @@
+import requests
+from mycroft import MycroftSkill, intent_handler
+from websocket import create_connection
+from mycroft.messagebus.message import Message
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
-from mycroft.messagebus.message import Message
-import time
 
 logger = getLogger(__name__)
 
 class Mindexpression(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
         super(Mindexpression, self).__init__(name="Mindexpression")
 
+    @intent_handler('Mindexpression.intent')
+    def handle_mindai(self, message):
+        self.speak_dialog('Mindexpression')
+
     def initialize(self):
-        # Set up the Mind Expression listener
-        self.add_event('mind-expression-skill:command', self.handle_command)
+        self.add_event('recognizer_loop:utterance',
+                       self.send_to_mind_expression)
+        self.url = self.settings.get('url')
+        self.api_key = self.settings.get('api_key')
 
-    def handle_command(self, message):
-        # Get the command from the Mind Expression message
-        command = message.data.get('command')
-
-        if command == 'wake_up':
-            # Send the wake up command to Mycroft
-            self.bus.emit(Message('mycroft.awoken'))
-
-        elif command == 'go_to_sleep':
-            # Send the go to sleep command to Mycroft
-            self.bus.emit(Message('mycroft.stop'))
-
-        elif command == 'set_volume':
-            # Get the volume value from the Mind Expression message
-            volume = message.data.get('volume')
-            # Set the Mycroft volume to the specified value
-            self.bus.emit(Message('mycroft.volume.set', {"percent": volume}))
-
-        elif command == 'play_music':
-            # Get the music to play from the Mind Expression message
-            music = message.data.get('music')
-            # Play the specified music using Mycroft's audio service
-            self.bus.emit(Message('mycroft.audio.service.play', {"tracks": [{"uri": music}]}))
-
-        elif command == 'stop_music':
-            # Stop the music currently playing
-            self.bus.emit(Message('mycroft.audio.service.stop'))
-
+    def send_message(self, message):
+        headers = {'Authorization': f'Bearer {self.api_key}'}
+        data = {'message': message}
+        response = requests.post(self.url, headers=headers, json=data)
+        if response.status_code == 200:
+            return response.json().get('response')
         else:
-            logger.warning("Received unknown command from Mind Expression: {}".format(command))
+            logger.warning(
+                f'Mind Expression request failed with status code {response.status_code}')
+            return None
+
+    def handle(self, message):
+        response = self.send_message(message.data['utterance'])
+        if response is not None:
+            self.speak(response)
+
+    def send_to_mind_expression(self, message):
+        try:
+            ws = create_connection("wss://localhost:8181")
+            utterance = message.data.get('utterance')
+            message = {
+                'type': 'speak',
+                'data': {'utterance': utterance}
+            }
+            ws.send(json.dumps(message))
+            response = ws.recv()
+            logger.info("Received response from Mind Expression: " + response)
+            ws.close()
+            self.bus.emit(Message("speak", {"utterance": response}))
+        except:
+            logger.error("Failed to connect to Mind Expression")
 
     def stop(self):
-        # Clean up the Mind Expression listener
-        self.remove_event('mind-expression-skill:command')
+        pass
 
 
 def create_skill():
